a0ed59226efb400a02e1f91519d33a8f9a075d47
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2024-11-18 13:19:03 +0530

    updated

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,36 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1 className='bg-green-400 text-black p-4 rounded -xl'>Tailwind Test</h1>
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://img.freepik.com/free-photo/portrait-man-cartoon-style_23-2151134017.jpg" alt="" width="384" height="512"/>
-  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
+      <h1 className="bg-green-400 text-black p-4 rounded -xl mb-4">Tailwind Test</h1>
+      <div className="w-60 h-60 rounded-xl">
+        <img
+          src="https://media1.giphy.com/media/z8n8dWgQ0mgEIyzlmV/giphy.gif?cid=790b7611a5ba988db1bc7457636dd163c28af6f6dbc84a77&rid=giphy.gif&ct=g"
+          alt="Giphy"
+          className="rounded-t-xl"
+        />
+        <div className="glass py-4 px-5 relative -top-[3.4rem]  rounded-b-xl z-10">
+          <h1 className="font-bold  font-mono  text-xl">Available soon</h1>
+        </div>
       </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
