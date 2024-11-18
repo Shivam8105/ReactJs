d5212124a78b49a493ce284f482b2147d29d31ee
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2024-11-18 23:50:05 +0530

    updated

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,11 +6,16 @@ import Card from "./components/card";
 
 function App() {
   const [count, setCount] = useState(0);
+  let myObj = {
+    username: "Shivam",
+    age: 20
+  }
 
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 rounded -xl mb-4">Tailwind Test</h1>
-     <Card/>
+     <Card username = "chai or code" btnText="click me"/>
+     <Card username = "Shivam" btnText="Visit me"/>
     </>
   );
 }
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-function Card() {
+function Card({username,btnText}) {
+    console.log(username);
   return (
     <>
       <div className="w-60 h-60 rounded-xl">
@@ -10,7 +11,7 @@ function Card() {
           className="rounded-t-xl"
         />
         <div className="glass py-4 px-5 relative -top-[3.4rem]  rounded-b-xl z-10">
-          <h1 className="font-bold  font-mono  text-xl">Available soon</h1>
+          <h1 className="font-bold  font-mono  text-xl">{btnText}</h1>
         </div>
       </div>
     </>
