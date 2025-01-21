fe3c1c59120d60aad779811c6e7ccb97e12b3e21
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-01-21 19:54:04 +0530

    updated

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Card from "./components/card";
 
@@ -15,7 +13,7 @@ function App() {
     <>
       <h1 className="bg-green-400 text-black p-4 rounded -xl mb-4">Tailwind Test</h1>
      <Card username = "chai or code" btnText="click me"/>
-     <Card username = "Shivam" btnText="Visit me"/>
+     {/* <Card username = "Shivam" btnText="Visit me"/> */}
     </>
   );
 }
