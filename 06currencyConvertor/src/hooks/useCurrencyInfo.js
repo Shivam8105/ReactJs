718628c7a370c5fc19fbbf5f2aa8fd4f6dbe6892
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-04-04 11:46:39 +0530

    updated

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -7,7 +7,6 @@ function useCurrencyInfo(currency){
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
     }, [currency])
-    console.log(data);
     return data;
 }
 
